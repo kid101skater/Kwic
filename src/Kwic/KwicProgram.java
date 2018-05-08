9f8ade8b182ca440d652997277972054bd5b0a16
kid101skater <klocke@mail.greenriver.edu>
2018-05-07 19:45:23 -0700

    Added part 1 structure

---
+++ src/Kwic/KwicProgram.java
@@ -0,0 +1,39 @@
+package Kwic;
+
+import java.util.ArrayList;
+
+public class KwicProgram
+{
+    // Setup our variables
+    private final ArrayList<String> allLines = new ArrayList<String>();
+    private final ArrayList<String> shiftedLines = new ArrayList<String>();
+    private final ArrayList<String> sortedShiftedLines = new ArrayList<String>();
+    private final String READ_FILE_PATH = "readfile.txt";
+    private final String OUTPUT_FILE_PATH = "output.txt";
+
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    public void readLinesFromFile()
+    {
+
+    }
+
+    public void shiftLines()
+    {
+
+    }
+
+    public void sortShiftedLines()
+    {
+
+    }
+
+    public void writeDataToFile()
+    {
+
+    }
+}
