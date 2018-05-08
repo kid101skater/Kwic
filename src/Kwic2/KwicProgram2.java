7b084a4cf418355e89d7fcf84d361c6bad9005a4
kid101skater <klocke@mail.greenriver.edu>
2018-05-07 20:51:11 -0700

    Added v2 full functionality

--- src/Kwic2/KwicProgram2.java
+++ src/Kwic2/KwicProgram2.java
@@ -1,30 +1,79 @@
 package Kwic2;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class KwicProgram2
 {
-    public ArrayList<Lines> lines = new ArrayList<Lines>();
+    private static final String READ_FILE_PATH = "readfile.txt";
+    private static  final String OUTPUT_FILE_PATH = "output.txt";
+    public static ArrayList<Lines> lines = new ArrayList<Lines>();
 
     public static void main(String[] args)
     {
-
+        readFileToObject();
+        sortLines();
+        writeToFile();
     }
 
-    public void ShiftLines()
+    public static void readFileToObject()
     {
-        // rewrite the lines
+        try
+        {
+            File file = new File(READ_FILE_PATH);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            String line;
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                Lines newLine = new Lines();
+                newLine.setOriginalLine(line);
+                lines.add(newLine);
+            }
+            fileReader.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
-    public ArrayList<Lines> sortLines()
+    public static void sortLines()
     {
-        // return sorted array of lines
-        return this.lines;
+        Collections.sort(lines, new Comparator<Lines>() {
+            @Override
+            public int compare(Lines l1, Lines l2) {
+                return l1.toString().compareTo(l2.toString());
+            }
+        });
     }
 
-    public void WriteToFile()
+    public static void writeToFile()
     {
+        File file = new File(OUTPUT_FILE_PATH);
+        if(file.exists())
+        {
+            file.delete();
+        }
+        try
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE_PATH));
+            for(int i = 0; i < lines.size(); i++)
+            {
+                String shiftedLine = lines.get(i).getShiftedLine();
+                System.out.println(shiftedLine);
+                writer.write(shiftedLine + "\n");
+            }
 
+            writer.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 
 
--- src/Kwic2/Lines.java
+++ src/Kwic2/Lines.java
@@ -4,4 +4,26 @@ public class Lines
 {
     public String originalLine;
     public String shiftedLine;
+
+    public String getOriginalLine()
+    {
+        return originalLine;
+    }
+
+    public void setOriginalLine(String originalLine)
+    {
+        this.originalLine = originalLine;
+        String line[] = originalLine.split(" ", 2);
+        shiftedLine = line[1] + " " + line[0];
+    }
+
+    public String getShiftedLine()
+    {
+        return shiftedLine;
+    }
+
+    public void setShiftedLine(String shiftedLine)
+    {
+        this.shiftedLine = shiftedLine;
+    }
 }
